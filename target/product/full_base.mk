7221e64afb427889ae7557390e4c1f29350e0e6c
Genkzsz11 <genkzsz11@gmail.com>
2021-11-07 21:56:10 +0700

    Don't check for google audio
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -50,12 +50,7 @@ PRODUCT_VENDOR_PROPERTIES := \
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
-# Get some sounds
-ifeq ($(NAD_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-endif
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
